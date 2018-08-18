85cb1d34e977afd561f5d2746a11164b66ff8422
Valentin Durst <ValentinDurst@gmail.com>
2018-08-18 20:21:57 +0200

    MoveResult introduced.

--- src/main/java/de/pandigo/games/first/controller/PlayerController.java
+++ src/main/java/de/pandigo/games/first/controller/PlayerController.java
@@ -2,8 +2,11 @@ package de.pandigo.games.first.controller;
 
 import de.pandigo.games.first.domain.client.MapClient;
 import de.pandigo.games.first.domain.client.PlayerClient;
+import de.pandigo.games.first.domain.entity.map.MoveResult;
 import de.pandigo.games.first.domain.entity.map.Position;
 import de.pandigo.games.first.domain.type.CustomMediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/player")
 public class PlayerController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlayerController.class);
+
     @Autowired
     private MapClient mapClient;
 
@@ -22,8 +27,11 @@ public class PlayerController {
 
     @RequestMapping(value = "/{playerId}", method = RequestMethod.POST, consumes = CustomMediaType.APPLICATION_MAP_OBJECTMOVETOPOSITION_JSON)
     public void movePlayerToPosition(@PathVariable(value = "playerId") final long playerId, @RequestBody() final Position position) {
-        this.mapClient.moveObjectToPosition(playerId, position);
+        final MoveResult moveResult = this.mapClient.moveObjectToPosition(playerId, position);
+        if (!moveResult.isSuccess()) {
+            LOGGER.debug("Can not move player {} from position {} to position {}", playerId, moveResult.getNewPosition(), position);
+        } else {
+            LOGGER.trace("Moved player {} to position {}", playerId, moveResult.getNewPosition());
+        }
     }
-
-
 }
