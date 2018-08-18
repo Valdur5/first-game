39b525dd4ff9141adefc49649d5d1f8e51ae2325
Valentin Durst <ValentinDurst@gmail.com>
2018-08-18 11:10:06 +0200

    Initial Commit.

---
+++ src/main/java/de/pandigo/games/first/Application.java
@@ -0,0 +1,33 @@
+package de.pandigo.games.first;
+
+import de.pandigo.games.first.domain.clients.PlayerClient;
+import de.pandigo.games.first.domain.entities.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    @Autowired
+    private PlayerClient playerClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            Player player = playerClient.createPlayer();
+            playerClient.getPlayer(player.getPlayerId());
+            player = playerClient.addExpToPlayer(player.getPlayerId(), 1000);
+            System.out.println(player.getLevel());
+            player = playerClient.addExpToPlayer(player.getPlayerId(), 1000);
+            player = playerClient.addExpToPlayer(player.getPlayerId(), 1000);
+            System.out.println(player.getLevel());
+        };
+    }
+}
